28fa1ef83643bfacbc92cd828041e30b3e0f1384
HaxTwoWater <haxtwowater@gmail.com>
2025-12-10 16:08:32 +0100

    features(main): initializing first server

--- main.go
+++ main.go
@@ -1,1 +1,17 @@
-package Groupie_Tracker
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func main() {
+	http.HandleFunc("/", indexHandler)
+
+	fmt.Println("Listening on port 8080")
+	http.ListenAndServe(":8080", nil)
+}
