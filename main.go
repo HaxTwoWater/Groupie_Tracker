9199cc1f6212669dd535735646d29002f9c3ca7f
HaxTwoWater <haxtwowater@gmail.com>
2025-12-12 14:11:29 +0100

    fix(serv): fonction to start  the serv done

--- main.go
+++ main.go
@@ -1,18 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"net/http"
-)
-
-func indexHandler(w http.ResponseWriter, r *http.Request) {
-		
-}
-
 func main() {
-	fs := http.FileServer(http.Dir("./"))
-	http.Handle("/", http.StripPrefix("/static/", fs))
-
-	fmt.Println("Listening on port 8080 http://localhost:8080")
-	http.ListenAndServe(":8080", nil)
+	server.Start()
 }
