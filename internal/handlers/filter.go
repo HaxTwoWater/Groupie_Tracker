dd1d9e39ad1d545209edba6912bc2d994de0ff3d
HaxTwoWater <haxtwowater@gmail.com>
2026-02-09 10:05:49 +0100

    feat(filter): added filter handler for the new filter template

---
+++ internal/handlers/filter.go
@@ -0,0 +1,21 @@
+package handlers
+
+import (
+	"net/http"
+
+	"groupie_tracker/internal/render"
+)
+
+func Filter(v *render.Render) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path != "/filter" {
+			w.WriteHeader(http.StatusNotFound)
+			v.Render(w, "error.html", map[string]any{
+				"Code":    404,
+				"Message": "Page introuvable",
+			})
+			return
+		}
+		v.Render(w, "filter.html", map[string]any{})
+	}
+}
